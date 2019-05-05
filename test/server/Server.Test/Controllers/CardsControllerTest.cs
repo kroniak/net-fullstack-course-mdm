abd34013fdef06fe1301aa71149835233f31448c
Crack123451 <Makc00756112345@gmail.com>
2019-05-06 00:48:06 +0300

    Запустил Stryker, исправил баг

--- test/server/Server.Test/Controllers/CardsControllerTest.cs
+++ test/server/Server.Test/Controllers/CardsControllerTest.cs
@@ -218,6 +218,8 @@ namespace Server.Test.Controllers
 
             Assert.IsType<BadRequestObjectResult>(getResult.Result);
             Assert.Equal(400, result.StatusCode);
+            Assert.True(_controller.ModelState.Keys.Contains("number"));
+            Assert.Equal("This card number is invalid", _controller.ModelState.Values.ElementAt(0).Errors.ElementAt(0).ErrorMessage);
             Assert.Null(getResult.Value);
         }
 
